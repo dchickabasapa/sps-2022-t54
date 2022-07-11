1862aed0a943f41d5bef169c84032fea32b7fc76
Arnav Shah <arnavshah4444@gmail.com>
2022-07-11 18:35:58 -0500

    Add console logging for the selection made

--- my-app/src/components/dropdown.js
+++ my-app/src/components/dropdown.js
@@ -3,11 +3,15 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import * as Constants from '../constants';
 
 function DropdownMenu() {
+    const handleSelect=(e)=>{
+        console.log(e);
+    }
   return (
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+    <DropdownButton id="dropdown-basic-button"
+    title="Dropdown button" onSelect={handleSelect}>
         {Constants.traditions.map((key) => {
             return (
-                <Dropdown.Item>{key.id}</Dropdown.Item>
+                <Dropdown.Item key={key.id} eventKey={key.id}>{key.id}</Dropdown.Item>
             )
         })}
     </DropdownButton>
