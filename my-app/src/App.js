07d08f743938222e6f1f4f4d0ab319a3b63a06a6
Arnav Shah <arnavshah4444@gmail.com>
2022-07-11 18:02:24 -0500

    Add another drop down menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Col, Container, Row } from 'react-bootstrap';
 import DropdownMenu from './components/dropdown';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <DropdownMenu></DropdownMenu>
+        <Container>
+          <Row>
+            <Col><DropdownMenu></DropdownMenu></Col>
+            <Col><DropdownMenu></DropdownMenu></Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
